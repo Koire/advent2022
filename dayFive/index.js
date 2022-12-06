d8f3477c9b59cb1bb826b76eb0178dda8b481ad5
Koire <pikewb@gmail.com>
2022-12-06 12:08:34 +0900

    fix: move functions have 300ms delay

--- dayFive/index.js
+++ dayFive/index.js
@@ -1,4 +1,6 @@
 import { css } from "../lib/css.mjs"
+import { sleep } from "../lib/sleep.mjs"
+import { range } from "../lib/arrayHelpers.mjs"
 
 const input = await fetch("input.txt")
                     .then(res => res.text())
@@ -43,7 +45,7 @@ const getIndexes = (move) => {
     return [amount, fromCol - 1, toCol - 1]
 }
 
-const crateCopy = crates.map(row => row.slice())
+const crateCopy = crates.map(row => [...row])
 
 //solution
 moves.forEach((move, idx) => {
@@ -52,23 +54,66 @@ moves.forEach((move, idx) => {
         crateCopy[toCol].push(crateCopy[fromCol].pop())
     }
 })
-console.log(crateCopy.map(row => row.pop().match(/\[(\w)\]/)[1]).join(""))
 
+const copyArray = arr => arr.map(row => [...row])
+
+const partTwo = crates.map(row => [...row])
 
 moves.forEach((move, idx) => {
     const [amount, fromCol, toCol] = getIndexes(move)
-    for ( let i = 0; i < amount; i++) {
-        setTimeout(() => {
-            const offset = crates[fromCol].length - 1 
-            const crate = crates[fromCol].pop()
-            const visCrate = document.getElementById(`crate-${fromCol}-${offset}`)
-            crates[toCol].push(crate)
-            const newOffset = crates[toCol].length - 1
-            visCrate.style.left = `${toCol * 40}px`
-            visCrate.style.bottom = `${ newOffset * 30}px`
-            visCrate.id = `crate-${toCol}-${newOffset}`
-        }, idx * 200 + i * 100)
-        
-    }  
+    const stack = partTwo[fromCol]
+    const remainder = stack.slice(0, stack.length - amount)
+    const moveStack = stack.slice(stack.length - amount)
+    partTwo[fromCol] = [...remainder]
+    partTwo[toCol].push(...moveStack)
+    
 })
+
+console.log(crateCopy.map(row => row.pop().match(/\[(\w)\]/)[1]).join(""))
+console.log(partTwo.map(row => row.pop().match(/\[(\w)\]/)[1]).join(""))
+
+const moveCrate = (fromCol, offset, toCol, newOffset) => {
+    const visCrate = document.getElementById(`crate-${fromCol}-${offset}`)
+    visCrate.style.left = `${toCol * 40}px`
+    visCrate.style.bottom = `${ newOffset * 30}px`
+    visCrate.id = `crate-${toCol}-${newOffset}`
+}
+
+const doPartTwo = () => {
+    const crateList = copyArray(crates)
+    moves.forEach((move, idx) => {
+        const [amount, fromCol, toCol] = getIndexes(move)
+        const stack = crateList[fromCol]
+        const remainder = stack.slice(0, stack.length - amount)
+        const moveStack = stack.slice(stack.length - amount)
+        crateList[fromCol] = [...remainder]
+        crateList[toCol].push(...moveStack)
+        moveStack.forEach((crate, idx2) => {
+            const offset = crateList[toCol].length - moveStack.length
+            setTimeout(() => {
                 
+            },  idx * 200 + i * 200)
+        })
+        
+    })
+}
+const startPromise = new Promise((res) => setTimeout(() => res("console.log")), 500)
+const doPartOne = async () => {
+    const crateList = copyArray(crates)
+    return moves.map((move) => {
+        const [amount, fromCol, toCol] = getIndexes(move)
+        return range(amount).map(() => {
+            const offset = crateList[fromCol].length - 1
+            const crate = crateList[fromCol].pop()
+            crateList[toCol].push(crate)
+            const newOffset = crateList[toCol].length - 1
+            return () => moveCrate(fromCol, offset, toCol, newOffset)
+        })
+    })
+    .flatMap(fn => fn)
+    .reduce((lastFn, currentFn) => {
+            return lastFn.then(() => new Promise((res) => setTimeout(() => res(currentFn()), 300)))
+    }, startPromise)
+}
+await doPartOne()
+console.log(doPartOne())
